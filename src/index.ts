f648e4890914f4cbb4d21188809d43486a947ac3
seuuuul <65261106+seuuuul@users.noreply.github.com>
2020-05-13 12:40:16 +0200

    Remove unused await

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Client, Message, VoiceChannel } from "discord.js";
 import ytdl from "ytdl-core";
 
-const TOKEN = "token here";
+const TOKEN = "token";
 const YOUTUBE_LINKS = [
   "https://youtu.be/FzVR_fymZw4",
   "https://youtu.be/9pdj4iJD08s",
@@ -19,6 +19,8 @@ client.on("message", async (message) => {
 });
 
 const procedure = async (voiceChannel: VoiceChannel) => {
+  console.log("New procedure.");
+  
   // Set up stream/dispatcher and play at a random time.
   let voiceConnection = await voiceChannel.join();
   let stream = ytdl(YOUTUBE_LINKS[Math.round(Math.random())]);
@@ -31,10 +33,7 @@ const procedure = async (voiceChannel: VoiceChannel) => {
     setTimeout(resolve, 10_000);
   });
 
-  // Use .end() instead of .destroy(), because this emits a 'finish' event
-  // which runs the _cleanup() method that should destroy all streams
-  // in StreamDispatcher.js.
-  await dispatcher.end();
+  dispatcher.end();
 
   return;
 };
